0b2b0d5392ce6d5e92529d84b9140f159bff59e1
Henry Wu Ou <hwou@ucsd.edu>
2019-02-28 10:47:20 -0800

    lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'mKTZELg4RaRk9KacqBgt9I4GyE9bPQRD';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.878833, -117.235912],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	//add marker on map
+	L.marker([32.878833, -117.235912]).addTo(map);
 }
